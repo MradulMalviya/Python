cc48af1c5fc97fea62c8f7b48110fbeaac989446
Mradul Malviya <mradul690@gmail.com>
2024-08-24 17:26:22 +0530

    Updated QR_Generator.py

--- Python_projects/QR_Generator.py
+++ Python_projects/QR_Generator.py
@@ -3,12 +3,13 @@
 import pyqrcode 
 from pyqrcode import QRCode 
   
-# String which represent the QR code 
+# String which represents the QR code 
+# Attaching my linkedin profile link here to generate QR.
 s = "https://www.linkedin.com/in/mradul-malviya-3b8a7418b/?trk=opento_sprofile_details"
   
 # Generate QR code 
 url = pyqrcode.create(s) 
   
-# Create and save the png file naming "myqr.png" 
+# Create and save the png file name "myqr.png" 
 #Scalable Vector Graphics
 url.svg("mylinkedin.svg", scale = 8) 
